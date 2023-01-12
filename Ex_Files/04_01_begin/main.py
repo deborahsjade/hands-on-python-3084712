ede7727429bd15495824c00ce95b9211c9b5ea86
Deborah Jade <deborahsjade@gmail.com>
2023-01-12 21:17:24 +0000

    Display population of Australia

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,6 +1,11 @@
 import requests
 
 response = requests.get(
-    "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
+    "http://api.worldbank.org/v2/countries/AUS/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
-last_twenty_years = response.json()[1][:20]
+last_hundred_years = response.json()[1][:100]
+
+for year in last_hundred_years:
+    display_width = year["value"] // 10_000_000
+    value_fmt = "{:,}".format(year["value"])
+    print(f'{year["date"]}: {value_fmt}', "=" * display_width)
